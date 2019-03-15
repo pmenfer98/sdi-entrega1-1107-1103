347976ce105f8dd2738297efcbe939f4c15fa3a4
Pablo <UO258082@uniovi.es>
2019-03-16 00:17:26 +0100

    Tests de registro implementados

--- sdi1107-lab-spring/src/main/java/com/uniovi/tests/WallapopTests.java
+++ sdi1107-lab-spring/src/main/java/com/uniovi/tests/WallapopTests.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -22,43 +23,8 @@ public class WallapopTests {
   }
 
   @Test
-  public void loginTest() throws Exception {
-    driver.get("http://localhost:8090/");
-    driver.findElement(By.linkText("Identificate")).click();
-    driver.findElement(By.name("username")).click();
-    driver.findElement(By.name("username")).clear();
-    driver.findElement(By.name("username")).sendKeys("lucasFernandez@gmail.com");
-    driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("holaaaa");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::button[1]")).click();
-    driver.findElement(By.name("username")).click();
-    driver.findElement(By.name("username")).clear();
-    driver.findElement(By.name("username")).sendKeys("pablo@uniovi.es");
-    driver.findElement(By.name("password")).click();
-    driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("holaaaaaaaaa");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::button[1]")).click();
-    driver.findElement(By.name("username")).click();
-    driver.findElement(By.name("username")).clear();
-    driver.findElement(By.name("username")).sendKeys("pablo@uniovi.es");
-    driver.findElement(By.name("password")).click();
-    driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("admin123");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::button[1]")).click();
-  }
-  
-  @Test
-  public void logoutTest() throws Exception {
-    driver.get("http://localhost:8090/home");
-    driver.findElement(By.linkText("Desconectar")).click();
-  }
-  
-  
-  
-  @Test
-  public void registerTest() throws Exception {
-    driver.get("http://localhost:8090/");
-    driver.findElement(By.linkText("Registrate")).click();
+  public void testLoginConDatosValidos() throws Exception {
+    driver.get("http://localhost:8090/signup");
     driver.findElement(By.name("name")).click();
     driver.findElement(By.name("name")).clear();
     driver.findElement(By.name("name")).sendKeys("Juan Carlos");
@@ -67,31 +33,61 @@ public class WallapopTests {
     driver.findElement(By.name("lastName")).sendKeys("Álvarez García");
     driver.findElement(By.name("email")).click();
     driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("juancaAlvarez@gmail.com");
-    driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("adiosss");
-    driver.findElement(By.name("passwordConfirm")).click();
-    driver.findElement(By.name("passwordConfirm")).clear();
-    driver.findElement(By.name("passwordConfirm")).sendKeys("jajajaja");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::button[1]")).click();
-    driver.findElement(By.name("name")).click();
-    driver.findElement(By.name("name")).clear();
-    driver.findElement(By.name("name")).sendKeys("Juan Carlos");
-    driver.findElement(By.name("lastName")).click();
-    driver.findElement(By.name("lastName")).clear();
-    driver.findElement(By.name("lastName")).sendKeys("Álvarez García");
-    driver.findElement(By.name("email")).click();
-    driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("juancaAlvarez@gmail.com");
+    driver.findElement(By.name("email")).sendKeys("UO123456@uniovi.es");
     driver.findElement(By.name("password")).click();
     driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("contraseña");
+    driver.findElement(By.name("password")).sendKeys("ContraseñaContra");
     driver.findElement(By.name("passwordConfirm")).click();
     driver.findElement(By.name("passwordConfirm")).clear();
-    driver.findElement(By.name("passwordConfirm")).sendKeys("contraseña");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Las contraseñas no coinciden'])[1]/following::button[1]")).click();
+    driver.findElement(By.name("passwordConfirm")).sendKeys("ContraseñaContra");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Confirmar contrase�a'])[1]/following::button[1]")).click();
   }
   
+  @Test
+  public void testRegisterNombreApellidoCorreoVacios() throws Exception {
+    driver.get("http://localhost:8090/signup");
+    driver.findElement(By.name("password")).click();
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("ContraseñaPrueba");
+    driver.findElement(By.name("passwordConfirm")).clear();
+    driver.findElement(By.name("passwordConfirm")).sendKeys("ContraseñaPrueba");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Confirmar contrase�a'])[1]/following::button[1]")).click();
+  }
+  
+  @Test
+  public void testRegisterContraseñasInvalidas() throws Exception {
+    driver.get("http://localhost:8090/signup");
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Marco");
+    driver.findElement(By.name("lastName")).clear();
+    driver.findElement(By.name("lastName")).sendKeys("Fernández Pérez");
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("UO278634@uniovi.es");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("ContraseñaC");
+    driver.findElement(By.name("passwordConfirm")).clear();
+    driver.findElement(By.name("passwordConfirm")).sendKeys("Contraseña");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Confirmar contrase�a'])[1]/following::button[1]")).click();
+  }
+  
+  @Test
+  public void testRegisterEmailExistente() throws Exception {
+    driver.get("http://localhost:8090/signup");
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Julián");
+    driver.findElement(By.name("lastName")).clear();
+    driver.findElement(By.name("lastName")).sendKeys("Bermúdez García");
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("pablo@uniovi.es");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("HOLAprueba");
+    driver.findElement(By.name("passwordConfirm")).clear();
+    driver.findElement(By.name("passwordConfirm")).sendKeys("HOLAprueba");
+    driver.findElement(By.name("passwordConfirm")).sendKeys(Keys.ENTER);
+  }
  
   
 }
